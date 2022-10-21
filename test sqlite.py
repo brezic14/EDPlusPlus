c21a1fd8ebd3c7b237e7e5b8caf3ca86f7e504a0
Scover <thediscover22450@gmail.com>
2022-10-21 23:03:26 +0200

    BDD + commentaires

--- test sqlite.py
+++ test sqlite.py
@@ -1,20 +1,21 @@
+# Bibliothèque sqlite pour interagir avec des bases de données en Python.
 import sqlite3
 
+# Établissement d'une connection avec le fichier de base de donnée.
 conn = sqlite3.connect("dbed.db")
+# Création du curseur, un objet qui permet de parcourir la BDD et d'exécuter des requêtes SQL.
 cur = conn.cursor()
 
+# On fait une jointure entre COURS et MATIERES. On récupère les noms des matières de l'élève 3 (Raphaël) qui ont pour type "Option".
+cur.execute('SELECT nom FROM COURS,MATIERES WHERE id_matiere = id AND id_eleve = 3 AND type = "Option"')
+# Équivalent à
+# cur.execute('SELECT nom FROM COURS INNER JOIN MATIERES ON id_matiere = id WHERE type = "Option"')
 
-def retrouver_matieres(id_eleve):
-"""Retourne toutes les matières d'un élève."""
-# On a besoin d'utiliser cette fonction au lieu d'une requête SQL car la relation ELEVE plusieurs clés étrangères pour chaque matière stockées sous la forme de texte.
-    cur.execute(f"SELECT id,id_matieres FROM ELEVES WHERE id = {id_eleve}")
-    matieres = []
-    for id_mat in str(cur.fetchone()[1]).split(','):
-        cur.execute(f"SELECT * from MATIERES WHERE id = {id_mat}")
-        matieres.append(cur.fetchone())
-    return matieres
+# fetchall() dépile toutes les lignes du résultat et on aplatit la liste de tuples retournée en utilisant la compréhension.
+print("Options de Raphaël : ", ", ".join(item for sublist in cur.fetchall() for item in sublist))
 
+# On sélectionne plusieurs attributs de la jointure NOTES/EVALUATIONS pour l'élève 3 (Raphaël) dans la matière 16 (NSI)
+cur.execute("SELECT titre,valeur,noteMax,coef, valeur FROM NOTES,EVALUATIONS WHERE id_evaluation=id AND id_eleve = 3 AND id_matiere = 16")
 
-# recuperer les valeurs des notes de l'eleve 2 (raphaël) en la matière 2 (NSI)
-cur.execute("SELECT valeur from NOTES where id_eleve = 2 AND NOTES.id_matiere = 2")
-print(cur.fetchall())
+# On utilise l'interpoliation de chaîne pour afficher le résultat.
+print("Notes de Raphaël en NSI : ", ", ".join(f"{m[0]} {m[1]}/{m[2]} ({m[3]})" for m in cur.fetchall()))
