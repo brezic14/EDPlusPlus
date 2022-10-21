a847946c56a4fa3af4f35f03acbb1d5a450e55a9
Scover <thediscover22450@gmail.com>
2022-10-21 10:40:20 +0200

    Commentaires

--- test sqlite.py
+++ test sqlite.py
@@ -3,7 +3,10 @@ import sqlite3
 conn = sqlite3.connect("dbed.db")
 cur = conn.cursor()
 
+
 def retrouver_matieres(id_eleve):
+"""Retourne toutes les matières d'un élève."""
+# On a besoin d'utiliser cette fonction au lieu d'une requête SQL car la relation ELEVE plusieurs clés étrangères pour chaque matière stockées sous la forme de texte.
     cur.execute(f"SELECT id,id_matieres FROM ELEVES WHERE id = {id_eleve}")
     matieres = []
     for id_mat in str(cur.fetchone()[1]).split(','):
@@ -12,6 +15,6 @@ def retrouver_matieres(id_eleve):
     return matieres
 
 
-# recuperer les valeurs des notes de l'eleve 2 en NSI
+# recuperer les valeurs des notes de l'eleve 2 (raphaël) en la matière 2 (NSI)
 cur.execute("SELECT valeur from NOTES where id_eleve = 2 AND NOTES.id_matiere = 2")
 print(cur.fetchall())
